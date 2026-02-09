ba156a39cca89a35c2e89f090ea3b6c37daf4851
eliseu daniel <eliseudaniel29@gmail.com>
2026-02-09 16:40:19 -0300

    first commit

---
+++ src/components/toobar/HeaderToolbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <q-toolbar class="center">
+      <q-toolbar-title>
+        teste
+      </q-toolbar-title>
+    </q-toolbar>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
