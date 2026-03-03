84a402848a2c551eeded42d4f450d3705dd59b4c
eliseu daniel <eliseudaniel29@gmail.com>
2026-03-03 17:39:12 -0300

    new design

--- src/components/toobar/HeaderToolbar.vue
+++ src/components/toobar/HeaderToolbar.vue
@@ -1,12 +1,14 @@
 <template>
     <q-toolbar class="center">
-      <q-toolbar-title>
-        teste
-      </q-toolbar-title>
+      <q-toolbar-title>HOME</q-toolbar-title>
+      <q-toolbar-title>COLEÇÕES</q-toolbar-title>
+      <q-toolbar-title>AGENDAR VISITA</q-toolbar-title>
+      <q-toolbar-title>BLOG</q-toolbar-title>
+      <q-toolbar-title>SOBRE NÓS</q-toolbar-title>
     </q-toolbar>
 </template>
 
-<script setup>
+<script setup lang="ts">
 
 </script>
 
